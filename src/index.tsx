e2d61830f6e72d37f54e827da6ea42756e541107
kaiorr <kaio.ribeiro@sistemavitto.com.br>
2021-06-19 17:39:14 -0300

    feat: Listando transaçoes e seeds

--- src/index.tsx
+++ src/index.tsx
@@ -7,9 +7,30 @@ createServer({
   models: {
     transaction: Model,
   },
-});
 
-createServer({
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Frelancer de website",
+          type: "deposit",
+          category: "Dev",
+          amount: 6000,
+          createdAt: new Date("2021-02-12 09:00:00"),
+        },
+        {
+          id: 2,
+          title: "Aluguel",
+          type: "withdraw",
+          category: "Casa",
+          amount: 1200,
+          createdAt: new Date("2021-02-05 09:00:00"),
+        },
+      ],
+    });
+  },
+
   routes() {
     this.namespace = "api";
 
