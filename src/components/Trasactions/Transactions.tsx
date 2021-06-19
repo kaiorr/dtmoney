f255961ba6fcb475d243d2259a07e5c4e374ee9b
kaiorr <kaio.ribeiro@sistemavitto.com.br>
2021-06-19 17:45:28 -0300

    feat: Formatando Valores

--- src/components/Trasactions/Transactions.tsx
+++ src/components/Trasactions/Transactions.tsx
@@ -36,9 +36,18 @@ export const Transactions = () => {
           {transactions.map((item) => (
             <tr key={item.id}>
               <td> {item.title} </td>
-              <td className={item.type}> {item.amount} </td>
+              <td className={item.type}>
+                {new Intl.NumberFormat("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                }).format(item.amount)}
+              </td>
               <td> {item.category} </td>
-              <td> {item.createdAt} </td>
+              <td>
+                {new Intl.DateTimeFormat("pt-BR").format(
+                  new Date(item.createdAt)
+                )}
+              </td>
             </tr>
           ))}
         </tbody>
